10abc15012e46415932478d4988f7631f3d33361
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-06-18 18:46:21 +0200

    updated

--- config/widgets.php
+++ config/widgets.php
@@ -3,11 +3,21 @@
 return ['Widgets' => [
 	//General widgets
 	'general' => [
-		'MeCms.Posts::search', 
-		'MeCms.Posts::categories', 
-		'MeCms.Posts::latest' => ['limit' => 10],
+		'MeCms.Pages::pages',
+        'MeCms.Photos::albums' => ['render' => 'form'],
+		'MeCms.Photos::latest' => ['limit' => 1],
 		'MeCms.Photos::random' => ['limit' => 1],
-		'MeCms.Pages::pages'
+		'MeCms.Posts::categories' => ['render' => 'form'],
+		'MeCms.Posts::latest' => ['limit' => 10],
+		'MeCms.Posts::months' => ['render' => 'form'],
+		'MeCms.Posts::search',
+		'MeCms.PostsTags::popular' => [
+			'limit' => 10,
+			'prefix' => '#',
+            'render' => 'cloud',
+			'shuffle' => TRUE,
+			'style' => ['maxFont' => 40, 'minFont' => 12]
+		],
 	],
 	//Specific widgets for the homepage. 
 	//If empty, will be used the default widget
